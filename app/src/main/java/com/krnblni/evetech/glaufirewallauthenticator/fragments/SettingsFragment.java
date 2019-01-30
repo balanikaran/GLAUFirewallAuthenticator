df7f7e868731230f96ce4bd232f5feabce171186
krnblni <karanbalani007@gmail.com>
2019-01-30 19:26:00 +0530

    misc. changes

--- app/src/main/java/com/krnblni/evetech/glaufirewallauthenticator/fragments/SettingsFragment.java
+++ app/src/main/java/com/krnblni/evetech/glaufirewallauthenticator/fragments/SettingsFragment.java
@@ -17,7 +17,6 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
